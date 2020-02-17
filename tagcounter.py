23eb29b0622e6cb6e3daeb899b44b8193c5d4b60
Pavel_Khudzik <pavel_khudzik@epam.com>
2020-02-17 11:06:30 +0300

    REcreate the project

--- tagcounter.py
+++ tagcounter.py
@@ -1,6 +1,20 @@
+import sqlite3
+
 """
 Main file for running the programm
     tagcounter                      -> run GUI versiom
     tagcounter --<command> options  -> run console version
+    
+DB
+    - имя сайта (домен второго уровня)
+    - url
+    - дата проверки
+    - данные о тэгах
+
 """
+
+conn = sqlite3.connect('tagcounter.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE tagcounter
+             (site text, url text, data_of_check text, tag_info real, price real)''')
 
